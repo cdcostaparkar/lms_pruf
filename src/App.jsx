40239d2708398d336ecaec4bae59ca82c5810556
chris <cdcosta@parkar.digital>
2025-01-31 00:15:17 +0530

    pruf-sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Courses from './pages/progress/Courses';
 import Login from './pages/login/Login';
+import Layout from './pages/CourseModules/layout';
 
 
 function App() {
   return (
     <>
-      <Login/>
+    <Layout/>
+      {/* <Login/> */}
       {/* <Courses /> */}
     </>
   );

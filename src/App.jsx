bfe266099377db85997fa1214e67214d5ee4be66
Vaishnavi <VKumari@parkar.digital>
2025-01-30 17:20:00 +0530

    add login and signup pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Courses from './pages/progress/Courses';
+import Login from './pages/login/Login';
 
 
 function App() {
   return (
     <>
-      <Courses />
+      <Login/>
+      {/* <Courses /> */}
     </>
   );
 }
